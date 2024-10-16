d4a75e07f94b212c38505733a6bd103da1a877b1
Endeavour <martin.wonders@unn.ac.uk>
2024-10-16 15:45:04 +0100

    Solution: Note, had to move string variable out of while

--- KV5043_Lab3_Exercises/main.cpp
+++ KV5043_Lab3_Exercises/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 //ToDo: Add the filestream header
+#include <fstream>
 //ToDo: Add the string stream header
+#include <sstream>
 #include "window.h"
 
 int main(int argc, char* argv[])
 {
-
 	//ToDo: Declare an input file stream object named inputFile
+	std::ifstream inputFile;
 		
 	//ToDo: Open the inputFile object using the "windowParameters.txt" file
+	inputFile.open("windowParameters.txt");
 	
-	if (/*ToDo: test the inputFile to make sure it's open*/)
+	if (inputFile.is_open())
 	{
 		std::string lineOfText;
 		int xPos = 0;
@@ -18,18 +21,35 @@ int main(int argc, char* argv[])
 		int width = 0;
 		int height = 0;
 
-		while (/*Use the getline() function to read a line from inputFile to the lineOfText string*/)
+		std::string windowName, strX, strY, strWidth, strHeight;
+
+		while (std::getline(inputFile, lineOfText))
 		{
 			//Todo: Declare a stringstream object named sStream to work lineOfText string
-			
-			std::string windowName, strX, strY, strWidth, strHeight;
+			std::stringstream sStream(lineOfText);
 			
 			/*ToDo: Use the getline() function with a delimiter to work on the sStream object and store each of the comma seperated
 			* values in the file to the five string variables decclared above
 			*/
-
+			std::getline(sStream, windowName, ',');
+			std::getline(sStream, strX, ',');
+			std::getline(sStream, strY, ',');
+			std::getline(sStream, strWidth, ',');
+			std::getline(sStream, strHeight, ',');
 			//ToDo: use the stoi() function to convert the strings to integers where required.
+			try
+			{
+				xPos = std::stoi(strX);
+				yPos = std::stoi(strY);
+				width = std::stoi(strWidth);
+				height = std::stoi(strHeight);
+			}
+			catch (std::invalid_argument e)
+			{
+				std::cerr << "Could not convert a string to an int: " << e.what() << std::endl;
+			}
 			
+
 		}
 
 		Window aWindow = Window(windowName, xPos, yPos, width, height);
